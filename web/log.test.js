4c057ae41a4da8aa294adec61d9cc79254bddf8e
Robik123445 <127778607+Robik123445@users.noreply.github.com>
2025-07-14 14:08:01 +0200

    Create log.test.js

---
+++ web/log.test.js
@@ -0,0 +1,11 @@
++const fs = require('fs');
++const path = require('path');
++const log = require('./log');
++
++test('writes to log.txt asynchronously', async () => {
++  const file = path.join(__dirname, 'log.txt');
++  if (fs.existsSync(file)) fs.unlinkSync(file);
++  await log('test entry');
++  const content = fs.readFileSync(file, 'utf8');
++  expect(content).toMatch(/test entry/);
++});
