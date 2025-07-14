cb2f0b6a3d04a601cdea83d5a191fb73a08c7387
Robik123445 <127778607+Robik123445@users.noreply.github.com>
2025-07-14 20:11:36 +0200

    Update __init__.py

--- cam_slicer/sender/__init__.py
+++ cam_slicer/sender/__init__.py
@@ -1,12 +1,16 @@
 diff --git a/cam_slicer/sender/__init__.py b/cam_slicer/sender/__init__.py
-index c6b862e33167934f9612f66b3c0012fe68850312..37fc463b1580dab10e348ff9f6a445fbb2bb8746 100644
+index 37fc463b1580dab10e348ff9f6a445fbb2bb8746..6392e739bcb2ddca5101b19346714c322f110259 100644
 --- a/cam_slicer/sender/__init__.py
 +++ b/cam_slicer/sender/__init__.py
-@@ -1,25 +1,26 @@
+@@ -1,26 +1,31 @@
  """Serial streaming utilities."""
  
--from .serial_stream import stream_gcode_live
-+from .serial_stream import stream_gcode_live, send_gcode_over_serial
+-from .serial_stream import stream_gcode_live, send_gcode_over_serial
++from .serial_stream import (
++    stream_gcode_live,
++    send_gcode_over_serial,
++    list_available_ports,
++)
  from .serial_streamer import stream_gcode_to_grbl
  from .grbl_streamer import stream_gcode_interactive
  from .live_gcode_streamer import LiveGcodeStreamer
@@ -28,5 +32,6 @@ index c6b862e33167934f9612f66b3c0012fe68850312..37fc463b1580dab10e348ff9f6a445fb
      "save_checkpoint",
      "load_checkpoint",
      "stream_gcode_with_feedback",
-+    "send_gcode_over_serial",
+     "send_gcode_over_serial",
++    "list_available_ports",
  ]
