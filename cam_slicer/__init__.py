4fac20286d17947b9a7840a5e672fa2d23a47f2d
Robik123445 <127778607+Robik123445@users.noreply.github.com>
2025-07-17 21:10:33 +0200

    Update __init__.py

--- cam_slicer/__init__.py
+++ cam_slicer/__init__.py
@@ -1,36 +1,5 @@
-@@ -25,50 +25,51 @@
- from .utils import (
-     blend_laser_mill,
-     schedule_multi_head_job,
-     prepare_hybrid_job,
-     clip_toolpath_to_regions,
-     Strategy,
-     load_strategy,
-     run_strategy,
-     apply_heightmap_to_gcode,
- )
- from .utils.batch_processor import run_macro_sequence, batch_process_toolpaths
- from .shapes import generate_adaptive_path
- from .ai.adaptive_steps import compute_adaptive_steps
- from .ai.surface_texturing import generate_surface_texture
- from .shapes import generate_spiral_helix
- from .shapes import morph_between_curves
- from .visualizer import (
-     preview_gcode,
-     parse_gcode,
-     render_surface_and_toolpath,
-     align_toolpath_to_surface,
-     simulate_motion_over_surface,
-     analyze_toolpath_vs_surface,
-     export_preview_image,
-     backplot_gcode,
- )
-+from .robotics.safety import validate_toolpath
+@@ -56,50 +56,51 @@
  from .importers import (
-     import_dwg,
-     import_obj,
-     import_stl,
-     analyze_dxf,
      analyze_stl,
      mesh_simplify,
      import_and_vectorize_image,
@@ -51,31 +20,71 @@
  )
  from .cli_wizard import cli
  
-@@ -347,26 +348,27 @@
-     "generate_carving_paths",
-     "generate_zlevel_roughing_paths",
-     "generate_zlevel_finishing_paths",
-     "generate_parallel_finishing_paths",
-     "generate_offset_finishing_paths",
-     "generate_high_polish_paths",
-     "generate_radial_finishing_paths",
-     "project_curves_to_surface",
-     "plan_mesh_operations",
-     "import_and_vectorize_image",
-     "import_svg",
-     "cli",
-     "create_app",
-     "Machine",
-     "MachineManager",
-     "Job",
-     "UserManager",
-     "User",
-     "setup_logging",
-     "setup_night_logging",
-     "profiled",
-     "parallel_map",
-     "feedrate_advisor",
-     "trajectory_cleaner",
-     "surface_comparator",
-+    "validate_toolpath",
-]
+ try:  # optional dependency
+     from .api_server import create_app
+ except Exception:  # pragma: no cover - missing FastAPI
+     create_app = None
+ from .sensors import generate_heightmap, export_heightmap_to_json
++from .probing import probe_heightmap
+ from .sender import (
+     stream_gcode_live,
+     stream_gcode_to_grbl,
+     LiveGcodeStreamer,
+     stream_gcode_with_feedback,
+     stream_with_recovery,
+     resume_job,
+     save_checkpoint,
+     load_checkpoint,
+ )
+@@ -223,50 +224,51 @@
+     "axis_limits",
+     "GeoFence",
+     "get_active_geofence",
+     "reset_active_geofence",
+     "assign_hybrid_operations",
+     "blend_additive_subtractive",
+     "blend_laser_mill",
+     "schedule_multi_head_job",
+     "prepare_hybrid_job",
+     "clip_toolpath_to_regions",
+     "Strategy",
+     "load_strategy",
+     "run_strategy",
+     "run_macro_sequence",
+     "batch_process_toolpaths",
+     "generate_adaptive_path",
+     "compute_adaptive_steps",
+     "generate_spiral_helix",
+     "morph_between_curves",
+     "get_macro",
+     "get_machine_config",
+     "ToolDatabase",
+     "ToolMaterialDB",
+     "generate_heightmap",
+     "export_heightmap_to_json",
++    "probe_heightmap",
+     "stream_gcode_live",
+     "stream_gcode_to_grbl",
+     "LiveGcodeStreamer",
+     "stream_gcode_with_feedback",
+     "stream_with_recovery",
+     "resume_job",
+     "save_checkpoint",
+     "load_checkpoint",
+     "optimize_toolpath_based_on_surface",
+     "optimize_toolpath",
+     "optimize_toolpath_sequence",
+     "optimize_toolpath_sequence_tsp",
+     "smooth_toolpath_corners",
+     "filter_small_arcs",
+     "optimize_high_speed_toolpath",
+     "detect_debris",
+     "add_debris_zones_from_image",
+     "plan_toolpath_avoiding_debris",
+     "AIFeedbackAdvisor",
+     "ToolpathPredictor",
+     "MaterialSimulator",
+     "ForcePredictor",
+     "simulate_force_profile",
+     "ChipCoolantAdvisor",
+     "apply_chip_evacuations",
