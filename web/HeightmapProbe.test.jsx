85f3dda86e8bd26aeb7806bb08c38ec235691e8b
Robik123445 <127778607+Robik123445@users.noreply.github.com>
2025-07-17 23:07:46 +0200

    Update HeightmapProbe.test.jsx

--- web/HeightmapProbe.test.jsx
+++ web/HeightmapProbe.test.jsx
@@ -2,7 +2,19 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import HeightmapProbe from './src/HeightmapProbe';
 
 beforeEach(() => {
-  global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve({ points: [[0,0,1],[0,1,2],[1,0,3],[1,1,4]] }) }));
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          points: [
+            [0, 0, 1],
+            [0, 1, 2],
+            [1, 0, 3],
+            [1, 1, 4],
+          ],
+        }),
+    })
+  );
 });
 
 test('starts probing and displays map', async () => {
