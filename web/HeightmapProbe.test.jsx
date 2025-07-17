9fbdbf0940082a505b620d0832489adff02f0266
Robik123445 <127778607+Robik123445@users.noreply.github.com>
2025-07-17 21:27:52 +0200

    Create HeightmapProbe.test.jsx

---
+++ web/HeightmapProbe.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import HeightmapProbe from './src/HeightmapProbe';
+
+beforeEach(() => {
+  global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve({ points: [[0,0,1],[0,1,2],[1,0,3],[1,1,4]] }) }));
+});
+
+test('starts probing and displays map', async () => {
+  render(<HeightmapProbe />);
+  fireEvent.click(screen.getByText('Start Probing'));
+  expect(global.fetch).toHaveBeenCalledWith('/probe_heightmap', expect.any(Object));
+});
