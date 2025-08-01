e64d8331f06dbf48d45c08c997b1934d19cd22e8
Robik123445 <127778607+Robik123445@users.noreply.github.com>
2025-08-02 00:19:20 +0200

    Update BugReportForm.test.jsx

--- web/BugReportForm.test.jsx
+++ web/BugReportForm.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import BugReportForm from './BugReportForm';
-jest.mock('./logger', () => jest.fn());
-import log from './logger';
+jest.mock('./src/log.js', () => jest.fn());
+import log from './src/log.js';
 
 test('logs bug report', () => {
   render(<BugReportForm />);
